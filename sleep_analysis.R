85af08c2c47d066162067647e92c12720d7c3b0a
Sarah Kolbe <kolbe.sarah@gmail.com>
2024-09-05 18:43:04 +0000

    Preliminary data cleaning

--- sleep_analysis.R
+++ sleep_analysis.R
@@ -5,10 +5,19 @@
 # Set up ----------
 
 library(tidyverse)
-library(sf)
+library(janitor)
 
 options(scipen = 999)
 
 # Load data ----------
-huckle <- read.csv("raw_data/data_export_20240904.csv") %>%
-  filter(., Type == "Sleep")
+huckle <- read_csv("raw_data/data_export_20240904.csv", na = c("yellow", "")) 
+
+# Prep data ----------
+huckle_clean <- huckle |>
+  clean_names() |>
+  filter(type == "Sleep") |>
+  mutate(starttime = hour(start) + minute(start)/60,
+         starttime2 = hms::hms(start))
+
+ggplot(huckle_clean, aes(x = starttime, y = duration)) +
+  geom_point()
